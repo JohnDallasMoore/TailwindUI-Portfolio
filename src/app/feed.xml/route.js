ddc207591ae0da9088dd440aca8ced9173330d97
Dallas Moore <moore.johndallas@gmail.com>
2023-09-15 13:14:54 -0500

    added new articles to blog section

--- src/app/feed.xml/route.js
+++ src/app/feed.xml/route.js
@@ -10,8 +10,8 @@ export async function GET(req) {
   }
 
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Dallas Moore',
+    email: 'moore.johndallas@gmail.com',
   }
 
   let feed = new Feed({
