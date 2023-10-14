5ce3924e324a0acf8dd6afd82d5efb5055c96a39
Dallas Moore <moore.johndallas@gmail.com>
2023-10-13 21:43:56 -0500

    updated resume link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -208,8 +208,8 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
+      <Button href="https://docs.google.com/document/d/1JV9zUFeIbNMx1_75C3PK571XGfNa_zkj0iWlTsGoQAE/edit?usp=sharing" variant="secondary" className="group mt-6 w-full">
+        View Resume
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
     </div>
